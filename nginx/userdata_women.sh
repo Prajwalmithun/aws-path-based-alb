e02957598137c89f8085347c432ef4e6494257f2
vanquisher <32311274+Prajwalmithun@users.noreply.github.com>
2024-06-10 15:45:21 -0600

    Create userdata_women.sh

---
+++ nginx/userdata_women.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Install nginx
+sudo dnf install -y nginx
+
+# Enable and start it 
+sudo systemctl enable nginx
+sudo systemctl start nginx 
+
+# Add the HTML content
+cat << EOF > /usr/share/nginx/html/women.html
+<!DOCTYPE html>
+<html>
+<head>
+<title> Women Section </title>
+<style>
+html { color-scheme: light dark; }
+body { width: 35em; margin: 0 auto;
+font-family: Tahoma, Verdana, Arial, sans-serif; }
+</style>
+</head>
+<body>
+<h1>This is Women Section!! </h1> 
+<p>If you see this page, the nginx web server is successfully installed and
+working. Further configuration is required.</p>
+
+<p>For online documentation and support please refer to
+<a href="http://nginx.org/">nginx.org</a>.<br/>
+Commercial support is available at
+<a href="http://nginx.com/">nginx.com</a>.</p>
+
+<p><em>Thank you for using nginx.</em></p>
+</body>
+</html>
+EOF
+
+# Make nginx redirections
+cat << EOF > /etc/nginx/conf.d/redirects.conf
+server {
+    listen 80;
+    server_name 127.0.0.1;  # Replace with your domain name or IP address
+
+    location /men {
+        rewrite ^/men$ /men.html permanent;
+    }
+
+    # Add other server configurations if needed
+}
+EOF
+
+# Restart the nginx 
+sudo sytemctl restart nginx
