c0e02e286c0901baa211eb128a3bfbe84a2e0642
vanquisher <32311274+Prajwalmithun@users.noreply.github.com>
2024-06-10 11:12:30 -0600

    Update and rename userdata to userdata.sh

--- men/userdata.sh
+++ men/userdata.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
+
 # Install the pre-requisistes 
 sudo dnf install -y python3-pip git
 sudo pip3 install Flask 
 
 # Bring those code files 
+cd /home/ec2-user/
 git clone https://github.com/Prajwalmithun/aws-path-based-alb.git
 
 # Change the directory to the project repo
